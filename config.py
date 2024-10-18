e911288d6e60e1df302f945a12be4c625b0b85a2
“[Imsara <[imsara256@gmail.com]>
2024-10-18 10:47:54 +0100

    added some more colors

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+'''
+AUTHOR: Imsara Samarasinghe
+EMAIL: imsara256@gmail.com
+'''
 # module imports
 import pygame
 
@@ -6,7 +10,7 @@ width, height = 1200, 800
 
 # create PyGame window
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("3D CAD Visualization")
+pygame.display.set_caption("3D Engine Visualisation")
 
 # colors
 COLORS = {'WHITE':(255, 255, 255),
@@ -17,9 +21,14 @@ COLORS = {'WHITE':(255, 255, 255),
           'GREY':(128, 128, 128),
           'FLOOR_COLOR':(54, 179, 216),
           'BACKGROUND_COLOR': (232, 228, 240),
+          'BACKGROUND_COLOR_2': (229,229,229),
           'P_RED': (255,179,186),
           'P_ORANGE': (255,223,186),
           'P_YELLOW': (255,255,186),
           'P_GREEN': (186,255,201),
-          'P_BLUE': (186,225,255)
+          'P_BLUE': (186,225,255),
+          'P_PINK': (255,212,229),
+          'SEAGREEN': (104,196,175),
+          'LAVENDAR': (219,220,255),
+          'SALMON': (249,167,167)
           }
