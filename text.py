a4d6394fd62659c1d80020b2f4b176be87811d61
“[Imsara <[imsara256@gmail.com]>
2024-10-22 15:34:20 +0100

    Changed background colors for text

--- text.py
+++ text.py
@@ -9,7 +9,7 @@ from config import COLORS, screen
 # zoom level text
 def print_zoom(viewer_distance):
     font = pygame.font.Font('freesansbold.ttf', 16)
-    text = font.render(f'Zoom: {100-viewer_distance}', True, COLORS['BLACK'], COLORS['BACKGROUND_COLOR'])
+    text = font.render(f'Zoom: {100-viewer_distance}', True, COLORS['BLACK'], COLORS['GREY'])
     textRect = text.get_rect()
     textRect.center = (50,10)
     screen.blit(text, textRect)
@@ -17,7 +17,7 @@ def print_zoom(viewer_distance):
 # angles text
 def print_angles(angle_x, angle_y, angle_z):
     font = pygame.font.Font('freesansbold.ttf', 16)
-    text = font.render(f'angle_x : {math.degrees(angle_x):.2f}° | angle_y : {math.degrees(angle_y):.2f}° | angle_z : {math.degrees(angle_z):.2f}°', True, COLORS['BLACK'], COLORS['BACKGROUND_COLOR'])
+    text = font.render(f'angle_x : {math.degrees(angle_x):.2f}° | angle_y : {math.degrees(angle_y):.2f}° | angle_z : {math.degrees(angle_z):.2f}°', True, COLORS['BLACK'], COLORS['GREY'])
     textRect = text.get_rect()
     textRect.center = (200,50)
     screen.blit(text, textRect)
