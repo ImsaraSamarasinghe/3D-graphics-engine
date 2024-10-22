9000c0e9a850848dfdadb51862e2fb239c1b05dc
“[Imsara <[imsara256@gmail.com]>
2024-10-22 13:53:53 +0100

    Removed all shapes

--- engine.py
+++ engine.py
@@ -12,6 +12,7 @@ from shapes import Cylinder, Cube, Torus, Sphere
 from config import screen, COLORS # IMPORT params
 from background import Axes, Floor # IMPORT Axes class
 from text import print_angles, print_zoom
+from physics import Physics3D
 
 # initialise
 pygame.init()
@@ -28,17 +29,6 @@ def main():
     ax = Axes() # axes object
     ground = Floor() # floor object
 
-    # shapes class
-    cube1 = Cube(center=[0,10,0], face_color=COLORS['P_ORANGE'])
-    cube2 = Cube(center=[0,6,0], face_color=COLORS['P_GREEN'])
-    cube3 = Cube(center=[0,2,0], face_color=COLORS['P_RED'])
-    cube4 = Cube(center=[4,2,0], face_color=COLORS['P_YELLOW'])
-    cube5 = Cube(center=[-4,2,0], face_color=COLORS['P_PINK'])
-    cube6 = Cube(center=[0,14,0], face_color=COLORS['LAVENDAR'])
-    cube7 = Cube(center=[-4,14,0], face_color=COLORS['SALMON'])
-    cube8 = Cube(center=[4,14,0], face_color=COLORS['SEAGREEN'])
-
-    ball = Sphere(center=[20,0,0])
 
     while run:
         screen.fill(COLORS['BACKGROUND_COLOR_2']) # screen background
@@ -48,11 +38,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # close simulation
                 run = False
+
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: # find left mouse click position
                 rotating = True
                 last_mouse_pos = pygame.mouse.get_pos()
+
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1: # find the left mouse button up
                 rotating = False
+
             if event.type == pygame.MOUSEMOTION and rotating: # calculate the angles based on mouse positions
                 current_mouse_pos = pygame.mouse.get_pos()
                 dx = current_mouse_pos[0]-last_mouse_pos[0]
@@ -63,7 +56,7 @@ def main():
                 angle_y += dx * 0.01
 
                 last_mouse_pos = current_mouse_pos
-            if event.type == pygame.MOUSEWHEEL: # change zoom based on the scrollwheel
+            if event.type == pygame.MOUSEWHEEL: # change the zoom based on the scrollwheel
                 viewer_distance += event.y
 
             # Check for the fullscreen toggle event
@@ -87,16 +80,6 @@ def main():
         # Draw ground
         ground.draw_floor(angle_x, angle_y, angle_z, viewer_distance)
 
-        # Draw using the defined vertices
-        cube1.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube2.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube3.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube4.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube5.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube6.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube7.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        cube8.draw_cube(angle_x, angle_y, angle_z, viewer_distance)
-        ball.draw_sphere(angle_x, angle_y, angle_z, viewer_distance)
 
         # draw axes
         ax.draw_axes(angle_x, angle_y, angle_z, viewer_distance)
